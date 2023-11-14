2570129d6bcf186eb49f113f37666e6cb4b7bfc3
Horquidia <horquidia.a.lima@gmail.com>
2023-11-13 23:02:46 -0300

    Orientação a Objetos no JavaScript

--- conceito-SPA/src/index.js
+++ conceito-SPA/src/index.js
@@ -1,3 +1,5 @@
+import './router.js'
+
 const routes = {
     "/": "conceito-SPA/pages/home.html",
     "/about": "conceito-SPA/pages/about.html",
@@ -6,25 +8,6 @@ const routes = {
 
 }
 
-function route(event) {
-    event = event || window.event
-    event.preventDefault()
-
-    window.history.pushState({}, "", event.target.href)
-
-    handle()
-}
-
-function handle(){
-    const {pathname} = window.location
-    const route = routes[pathname] || routes[404]
-
-    fetch(route)
-        .then(data => data.text())
-        .then(html => {
-            document.querySelector("#app").innerHTML = html
-        })
-}
 
 handle()
 
